76d298b6d78df3d85bcf0468044ab259f85ba3e8
Lulu Nwenyi <dev@lulunwenyi.com>
2021-08-10 14:04:51 +0100

    postgres issue fixed

--- api/config.py
+++ api/config.py
@@ -41,7 +41,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 

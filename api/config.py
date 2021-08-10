a158836bc676c4d6a50a272c0f24ab1a85c0e93e
Lulu Nwenyi <dev@lulunwenyi.com>
2021-08-10 14:08:12 +0100

    db uri

--- api/config.py
+++ api/config.py
@@ -41,11 +41,8 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    
 #ENV CONFiG
 
 config = {

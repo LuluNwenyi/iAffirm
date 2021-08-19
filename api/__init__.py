63beb0eab2573690955b7829300e165015f2509b
Lulu Nwenyi <dev@lulunwenyi.com>
2021-08-19 11:55:40 +0100

    App Manager

--- api/__init__.py
+++ api/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import config, ProductionConfig
 
+# APP CONFIG
 app = Flask(__name__)
 app.config.from_object(ProductionConfig)
 db = SQLAlchemy(app)

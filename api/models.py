ecd6466feafdb94e0503c224bea3196f434aa533
Lulu Nwenyi <dev@lulunwenyi.com>
2021-09-01 01:52:39 +0100

    Database Models

--- api/models.py
+++ api/models.py
@@ -13,3 +13,27 @@ class Affirmation(db.Model):
         return {"text": self.text}
 
     db.create_all()
+
+
+
+# ADMiN TABLE
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    email = db.Column(db.String)
+
+    def to_json(self):
+        return {"username": self.username, "email": self.email}
+
+    db.create_all()
+
+
+
+# TOKEN BLOCKLiST TABLE
+class TokenBlocklist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    db.create_all()

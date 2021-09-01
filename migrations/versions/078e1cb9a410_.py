ea843bbb25b9f9b054399057dbf59bbc09e3fd35
Lulu Nwenyi <dev@lulunwenyi.com>
2021-09-01 21:23:56 +0100

    Database Migrations

---
+++ migrations/versions/078e1cb9a410_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 078e1cb9a410
+Revises: 
+Create Date: 2021-09-01 21:22:14.990992
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '078e1cb9a410'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

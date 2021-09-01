bec6f48a8dc423c29c152ce97ee2387904b33ba7
Lulu Nwenyi <dev@lulunwenyi.com>
2021-09-01 02:20:39 +0100

    Endpoint Decorators

--- api/decorators.py
+++ api/decorators.py
@@ -4,15 +4,15 @@
 from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
 from functools import wraps
-from .models import User
+from .models import Admin
 
 # ADMIN DECORATOR
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         current_user = get_jwt_identity()
-        user = User.query.filter_by(admin=True, username=current_user).first()
-        if not user:
+        admin = Admin.query.filter_by(admin=True, username=current_user).first()
+        if not admin:
             return jsonify({'message': 'You are not an admin!'}), 401
         return f(*args, **kwargs)
     return decorated_function

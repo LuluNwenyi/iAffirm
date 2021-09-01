52f0708c984064c62ec4e19581b8663d45792161
Lulu Nwenyi <dev@lulunwenyi.com>
2021-09-01 02:32:04 +0100

    App Manager

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from .config import config, ProductionConfig
 
 # APP CONFIG
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(ProductionConfig)
 db = SQLAlchemy(app)
 mig = Migrate()
+jwt = JWTManager(app)
 
 def create_app(config_name):
 
@@ -21,6 +23,7 @@ def create_app(config_name):
     # INIT EXTERNAL MODULES
     db.init_app(app)
     mig.init_app(app, db)
+    jwt.init_app(app)
 
     # REGISTER BLUEPRINTS
 
